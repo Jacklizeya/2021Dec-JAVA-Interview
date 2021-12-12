66d2e1bf12f8c0560dd258f310588d468ca8b779
Jack Zeya Li <74479553+Jacklizeya@users.noreply.github.com>
2021-12-11 20:27:02 -0700

    Update 20211211 IterativeQuickSort.java

--- 20211211 IterativeQuickSort.java
+++ 20211211 IterativeQuickSort.java
@@ -1,3 +1,6 @@
+// Partition will return the place left smaller and right bigger
+
+
 public int partition(int[] arr, int leftPointer, int rightPointer) {
   int pivot = arr[Math.floorDiv((leftPointer + rightPointer), 2)];
  
@@ -14,3 +17,20 @@ public int partition(int[] arr, int leftPointer, int rightPointer) {
   }
   return leftPointer;
 }
+
+
+public int[] quickSort(int[] arr, int start, int end) {
+    if (end > start) {
+    int partition = partition(arr, start, end);
+    // recurse on left partition
+      if (start < partition) {
+      quickSort(arr, start, partition);
+    }
+    // recurse on right partition
+    if (partition + 1 < end) {
+      quickSort(arr, partition + 1, end);
+    }
+  }
+  return arr;
+}
+ 
