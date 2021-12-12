22a81dc90a877871e5c6608612334005291fc3b3
Jack Zeya Li <74479553+Jacklizeya@users.noreply.github.com>
2021-12-11 20:15:56 -0700

    Create 20211211 IterativeQuickSort.java

---
+++ 20211211 IterativeQuickSort.java
@@ -0,0 +1,16 @@
+public int partition(int[] arr, int leftPointer, int rightPointer) {
+  int pivot = arr[Math.floorDiv((leftPointer + rightPointer), 2)];
+ 
+  while (leftPointer < rightPointer) {
+    while (arr[leftPointer] < pivot) {
+      leftPointer++;
+    }
+    while (arr[rightPointer] > pivot) {
+      rightPointer--;
+    }
+    if (leftPointer < rightPointer) {
+      swap(arr, leftPointer, rightPointer);
+    }
+  }
+  return leftPointer;
+}
