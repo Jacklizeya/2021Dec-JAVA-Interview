05bd79b29455d51219fb2c84852725c1a37911f1
Jack Zeya Li <74479553+Jacklizeya@users.noreply.github.com>
2021-12-19 17:10:57 -0700

    Update 20211219 TwoSum.java

--- 20211219 TwoSum.java
+++ 20211219 TwoSum.java
@@ -1,3 +1,22 @@
+// Solution 1: less optimal
+
+import java.util.*;
+
+class Program {
+  public static int[] twoNumberSum(int[] array, int targetSum) {
+    // Write your code here.
+		for (int i = 0; i < array.length - 1; i++) {
+			for (int j = i + 1; j < array.length; j++) {
+				if (array[i] + array[j] == targetSum) {return new int[] {array[i], array[j]};}
+			}
+		}
+		
+		
+		
+    return new int[0];
+  }
+}
+
 import java.util.*;
 
 class Program {
@@ -12,3 +31,26 @@ class Program {
 		return new int[0];
   }
 }
+
+// Time: o(N) Space: O(N)
+
+// Solution 3
+import java.util.*;
+
+class Program {
+  public static int[] twoNumberSum(int[] array, int targetSum) {
+    // Write your code here.
+		Arrays.sort(array);
+		int leftPointer = 0;
+		int rightPointer = array.length - 1;
+		while (leftPointer < rightPointer) {
+			if (array[leftPointer] + array[rightPointer] < targetSum) {leftPointer++;}
+			else if (array[leftPointer] + array[rightPointer] > targetSum) {rightPointer--;}
+			else {return new int[] {array[leftPointer], array[rightPointer]};}
+		}
+		
+    return new int[0];
+  }
+}
+
+// time n * logn, space: 1
