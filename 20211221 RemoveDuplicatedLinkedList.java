a616a9f33f31a6e0179f6f07e5c2b2cabeb4599e
Jack Zeya Li <74479553+Jacklizeya@users.noreply.github.com>
2021-12-21 21:15:08 -0700

    Create 20211221 RemoveDuplicatedLinkedList.java

---
+++ 20211221 RemoveDuplicatedLinkedList.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Program {
+  // This is an input class. Do not edit.
+  public static class LinkedList {
+    public int value;
+    public LinkedList next;
+
+    public LinkedList(int value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+
+  public LinkedList removeDuplicatesFromLinkedList(LinkedList linkedList) {
+    // Write your code here.
+		LinkedList headNode = linkedList;
+		LinkedList currentNode = linkedList;
+		while (currentNode.next != null) {
+			if (currentNode.next.value == currentNode.value) {
+				currentNode.next = currentNode.next.next;
+			} else {
+				currentNode = currentNode.next;
+			}
+		}		
+    return headNode;
+  }
+}
