d3dcf718c07876433222b7f4607ecf13c15f7728
Jack Zeya Li <74479553+Jacklizeya@users.noreply.github.com>
2021-12-31 10:37:39 -0800

    Update 20211231 YoungestAncestor.java

--- 20211231 YoungestAncestor.java
+++ 20211231 YoungestAncestor.java
@@ -43,3 +43,64 @@ class Program {
     }
   }
 }
+
+
+import java.util.*;
+
+class Program {
+  public static AncestralTree getYoungestCommonAncestor(
+      AncestralTree topAncestor, AncestralTree descendantOne, AncestralTree descendantTwo) {
+    // Write your code here.
+	  int depthOne = getDepthFromRoot(descendantOne, topAncestor);
+		int depthTwo = getDepthFromRoot(descendantTwo, topAncestor);
+		AncestralTree pointerOne = descendantOne;
+		AncestralTree pointerTwo = descendantTwo;
+		if (depthOne > depthTwo) {
+			int target = depthOne - depthTwo;
+			while (target >= 1) {
+				pointerOne = pointerOne.ancestor;
+				target--;
+			}
+		} else {
+			int target = depthTwo - depthOne;
+			while (target >= 1) {
+				pointerTwo = pointerTwo.ancestor;
+				target--;
+			}
+		}
+		
+		while(pointerOne != pointerTwo) {
+			pointerOne = pointerOne.ancestor;
+			pointerTwo = pointerTwo.ancestor;
+		}
+		
+		return pointerOne;
+  }
+	
+	public static int getDepthFromRoot(AncestralTree leafNode, AncestralTree rootNode) {
+		AncestralTree currentNode = leafNode;
+		int count = 0;
+		while (currentNode != null) {
+			currentNode = currentNode.ancestor;
+			count++;
+		}
+		return count;
+	}
+
+  static class AncestralTree {
+    public char name;
+    public AncestralTree ancestor;
+
+    AncestralTree(char name) {
+      this.name = name;
+      this.ancestor = null;
+    }
+
+    // This method is for testing only.
+    void addAsAncestor(AncestralTree[] descendants) {
+      for (AncestralTree descendant : descendants) {
+        descendant.ancestor = this;
+      }
+    }
+  }
+}
